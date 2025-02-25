fdd3e09de513e8fe7286680258ddddd546d22f90
mdshahed89 <mdshahedprofile@gmail.com>
2025-02-25 21:22:34 +0600

    trying to solve vps crash issue

--- backend/routes/candidateRoutes.js
+++ backend/routes/candidateRoutes.js
@@ -3,46 +3,52 @@ const router = express.Router();
 const Candidate = require("../models/Candidate");
 const nodemailer = require("nodemailer");
 const { protect } = require("../middleware/authMiddleware");
-const User = require("../models/user")
+const User = require("../models/user");
 const upload = require("../config/cloudinary");
 const Jobs = require("../models/Jobs");
-const Company = require("../models/Company")
+const Company = require("../models/Company");
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    host: "send.one.com", // one.com SMTP server
-    port: 587, // Secure port for SSL
-    secure: false, // SSL for encryption
+    host: "send.one.com",
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_TEAM, // Your one.com email
-      pass: process.env.EMAIL_TEAM_PASS, // Your one.com password (from environment variables)
+      user: process.env.EMAIL_TEAM,
+      pass: process.env.EMAIL_TEAM_PASS,
     },
   });
 };
 
 router.get("/jobs/:id", async (req, res) => {
   try {
-    const {id} = req.params
-    if(!id){
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(400).json({
         success: false,
-        message: "Job Id is required"
-      })
+        message: "Job Id is required",
+      });
     }
-    const job = await Jobs.findById(id)
-    if(!job){
-      return res.status(404).json({ success: false,message: "Job not found" });
+    const job = await Jobs.findById(id);
+    if (!job) {
+      return res.status(200).json({
+        success: true,
+        message: "Job not found",
+        data: {}, 
+      });
     }
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       message: "Job found successfully",
-      data: job
-    })
+      data: job,
+    });
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ success: false, message: "Server Error when getting job" });
+    return res
+      .status(500)
+      .json({ success: false, message: "Server Error when getting job" });
   }
-} )
+});
 
 router.post("/", protect, async (req, res) => {
   const { campaignInfo, customerInfo, quizzes } = req.body;
@@ -50,16 +56,13 @@ router.post("/", protect, async (req, res) => {
   if (!campaignInfo || !customerInfo || !quizzes) {
     return res.status(400).json({ message: "Missing required fields" });
   }
-console.log(req.user);
+  console.log(req.user);
 
   try {
+    const user = await User.findById(req?.user?.id);
 
-    const user = await User.findById(req?.user?.id)
-
-    if(!user){
-      res
-      .status(404)
-      .json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     const newCandidate = new Candidate({
@@ -70,81 +73,77 @@ console.log(req.user);
     });
 
     await newCandidate.save();
-    res
+    return res
       .status(201)
       .json({ message: "Candidate added successfully", newCandidate });
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ message: "Server Error", error: error.message });
+    return res.status(500).json({ message: "Server Error", error: error.message });
   }
 });
 
 router.post("/create-job", upload.single("resume"), async (req, res) => {
   try {
-    const {customerInfo, questions, answers, dashboardId} = req.body
-    if(!customerInfo || !questions || !answers || !dashboardId){
+    const { customerInfo, questions, answers, dashboardId } = req.body;
+    if (!customerInfo || !questions || !answers || !dashboardId) {
       return res.status(400).json({
         success: false,
-        message: "Required field missing"
-      })
+        message: "Required field missing",
+      });
     }
 
     console.log("info", customerInfo);
 
     const parsedCustomerInfo = JSON.parse(req.body.customerInfo);
-    
+
     const newCandidate = new Candidate({
       customerInfo: parsedCustomerInfo,
       questions,
       answers,
-      dashboardId
-    })
+      dashboardId,
+    });
 
     if (req?.file) {
       newCandidate.resumeUrl = req.file?.path;
       console.log(req.file?.path);
     }
 
-    newCandidate.save()
+    newCandidate.save();
 
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       message: "Candidate created successfully",
-      data: newCandidate
-    })
+      data: newCandidate,
+    });
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ success: false,message: "Server Error of creat-job" });
+    return res
+      .status(500)
+      .json({ success: false, message: "Server Error of creat-job" });
   }
-})
+});
 
 router.get("/", protect, async (req, res) => {
   try {
+    const user = await User.findById(req?.user?.id);
 
-    const user = await User.findById(req?.user?.id)
-
-    if(!user){
-      res
-      .status(404)
-      .json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     const candidates = await Candidate.find({ dashboardId: user?.dashboardId });
-    res.status(200).json(candidates);
+    return res.status(200).json(candidates);
   } catch (error) {
-    res.status(500).json({ message: "Server Error", error: error.message });
+    return res.status(500).json({ message: "Server Error", error: error.message });
   }
 });
 
 router.get("/:id", protect, async (req, res) => {
   try {
+    const user = await User.findById(req?.user?.id);
 
-    const user = await User.findById(req?.user?.id)
-
-    if(!user){
-      res
-      .status(404)
-      .json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     console.log("Candidate ID:", req.params.id);
@@ -161,10 +160,10 @@ router.get("/:id", protect, async (req, res) => {
       return res.status(404).json({ message: "Candidate not found" });
     }
 
-    res.status(200).json(candidate);
+    return res.status(200).json(candidate);
   } catch (error) {
     console.error("Error fetching candidate:", error.message);
-    res.status(500).json({ message: "Server Error", error: error.message });
+    return res.status(500).json({ message: "Server Error", error: error.message });
   }
 });
 
@@ -172,13 +171,10 @@ router.put("/:id/job-match", protect, async (req, res) => {
   const { progress } = req.body;
 
   try {
+    const user = await User.findById(req?.user?.id);
 
-    const user = await User.findById(req?.user?.id)
-
-    if(!user){
-      res
-      .status(404)
-      .json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     const candidate = await Candidate.findOne({
@@ -192,11 +188,11 @@ router.put("/:id/job-match", protect, async (req, res) => {
 
     candidate.jobMatchProgress = progress;
     await candidate.save();
-    res
+    return res
       .status(200)
       .json({ message: "Job match progress updated successfully", candidate });
   } catch (error) {
-    res.status(500).json({ message: "Server Error", error: error.message });
+    return res.status(500).json({ message: "Server Error", error: error.message });
   }
 });
 
@@ -204,13 +200,10 @@ router.put("/:id/position", protect, async (req, res) => {
   const { columnPosition } = req.body;
 
   try {
+    const user = await User.findById(req?.user?.id);
 
-    const user = await User.findById(req?.user?.id)
-
-    if(!user){
-      res
-      .status(404)
-      .json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     const candidate = await Candidate.findOne({
@@ -224,12 +217,12 @@ router.put("/:id/position", protect, async (req, res) => {
 
     candidate.columnPosition = columnPosition;
     await candidate.save();
-    res.status(200).json({
+    return res.status(200).json({
       message: "Candidate column position updated successfully",
       candidate,
     });
   } catch (error) {
-    res.status(500).json({ message: "Server Error", error: error.message });
+    return res.status(500).json({ message: "Server Error", error: error.message });
   }
 });
 
@@ -241,10 +234,9 @@ router.post("/send-email", protect, async (req, res) => {
   }
 
   try {
+    const cnd = await Candidate.findById(candidateId);
 
-    const cnd = await Candidate.findById(candidateId)
-
-    const company = await Company.findById(cnd?.dashboardId)
+    const company = await Company.findById(cnd?.dashboardId);
 
     const transporter = createTransporter();
 
@@ -257,12 +249,10 @@ router.post("/send-email", protect, async (req, res) => {
     };
     await transporter.sendMail(mailOptions);
 
-    const user = await User.findById(req?.user?.id)
+    const user = await User.findById(req?.user?.id);
 
-    if(!user){
-      res
-      .status(404)
-      .json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     const candidate = await Candidate.findOne({
@@ -277,16 +267,15 @@ router.post("/send-email", protect, async (req, res) => {
     candidate.questions = questions;
     await candidate.save();
 
-    res.status(200).json({ message: "Email sent and questions saved" });
+    return res.status(200).json({ message: "Email sent and questions saved" });
   } catch (error) {
-    res.status(500).json({ message: "Error sending email", error });
+    return res.status(500).json({ message: "Error sending email", error });
   }
 });
 
 router.get("/:id/get-questions", async (req, res) => {
   try {
-
-    const {id} = req.params
+    const { id } = req.params;
     // const user = await User.findById(req?.user?.id)
 
     // if(!user){
@@ -300,27 +289,24 @@ router.get("/:id/get-questions", async (req, res) => {
     //   dashboardId: user?.dashboardId,
     // });
 
-    const candidate = await Candidate.findById(id)
+    const candidate = await Candidate.findById(id);
 
     if (!candidate) {
       return res.status(404).json({ message: "Candidate not found" });
     }
 
-    res.status(200).json({ questions: candidate.questions });
+    return res.status(200).json({ questions: candidate.questions });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error });
+    return res.status(500).json({ message: "Server error", error });
   }
 });
 
 router.get("/:id/screening-answers", protect, async (req, res) => {
   try {
+    const user = await User.findById(req?.user?.id);
 
-    const user = await User.findById(req?.user?.id)
-
-    if(!user){
-      res
-      .status(404)
-      .json({ message: "User not found" });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     const candidate = await Candidate.findOne({
@@ -332,14 +318,14 @@ router.get("/:id/screening-answers", protect, async (req, res) => {
       return res.status(404).json({ message: "Candidate not found" });
     }
 
-    res.status(200).json({
+    return res.status(200).json({
       questions: candidate.questions || [],
       answers: candidate.answers || [],
       resumeUrl: candidate.resumeUrl || "",
     });
   } catch (error) {
     console.error("Server error:", error);
-    res.status(500).json({ message: "Server error", error });
+    return res.status(500).json({ message: "Server error", error });
   }
 });
 
@@ -349,17 +335,16 @@ router.post(
   async (req, res) => {
     const { questions, answers } = req.body;
     const candidateId = req.params.id;
-    console.log("working")
-    
-    try {
-      
-    //   const user = await User.findById(req?.user?.id)
+    console.log("working");
 
-    // if(!user){
-    //   res
-    //   .status(404)
-    //   .json({ message: "User not found" });
-    // }
+    try {
+      //   const user = await User.findById(req?.user?.id)
+
+      // if(!user){
+      //   res
+      //   .status(404)
+      //   .json({ message: "User not found" });
+      // }
 
       const candidate = await Candidate.findOne({
         _id: candidateId,
@@ -376,17 +361,16 @@ router.post(
       if (req?.file) {
         candidate.resumeUrl = req.file?.path;
         console.log(req.file?.path);
-        
       }
 
       await candidate.save();
 
-      res
+      return res
         .status(200)
         .json({ message: "Answers and resume saved successfully" });
     } catch (error) {
       console.error("Error saving candidate data:", error);
-      res.status(500).json({ message: "Server error", error });
+      return res.status(500).json({ message: "Server error", error });
     }
   }
 );
